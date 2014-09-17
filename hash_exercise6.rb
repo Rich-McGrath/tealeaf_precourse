38202b840b09b217c03fb1adb738dd3ec1c6911f
Rich-McGrath <richmcgrath@gmail.com>
2014-09-17 11:32:40 -0400

    Fixed typo

--- hash_exercise6.rb
+++ hash_exercise6.rb
@@ -12,7 +12,7 @@ result = {}
 words.each do |word|
   key = word.split('').sort.join
   if result.has_key?(key)
-    result|key|.push(word)
+    result[key].push(word)
   else
     result[key] = [word]
   end
