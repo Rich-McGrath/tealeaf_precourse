0450084ed764fedf7f355856cc72dd39f4b9bc43
Rich-McGrath <richmcgrath@gmail.com>
2015-06-14 19:04:51 -0400

    fixed syntax

--- practice_each.rb
+++ practice_each.rb
@@ -1,10 +1,9 @@
 # practice_each.rb
 
 names = ['Bob', 'Joe', 'Steve', 'Janice', 'Susan', 'Helen']
-x =1
+x = 1
 
-names.each do |name| 
+names.each do |name|
   puts "#{x}. #{name}"
   x +=1
   end
-   
