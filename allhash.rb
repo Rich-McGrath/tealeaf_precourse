c2ca4a74096634925570a90f8fa689e0a46d6e9f
Rich-McGrath <richmcgrath@gmail.com>
2015-06-28 15:22:41 -0400

    added book soultion as a commented out example

--- allhash.rb
+++ allhash.rb
@@ -1,7 +1,17 @@
-video_games = {videogamename: 'Bioshock', criticrating: '4 out of 5', videogamename: 'Mass Effect 3', criticrating: '4 out of 5'}
+video_games = {videogamename: 'Bioshock', criticrating: '4 out of 5', videogamename2: 'Mass Effect 3', criticrating2: '4 out of 5'}
 
 video_games.each_key { |key| puts key}
 
 video_games.each_value{ |value| puts value }
 
-video_games.each { |key, value| puts "The opposite of #{key} is #{value}" }
+video_games.each { |key, value| puts "#{key}  #{value}" }
+
+
+# Below is the solution from the book
+#opposites = {positive: "negative", up: "down", right: "left"}
+
+#opposites.each_key { |key| puts key }
+
+#opposites.each_value { |value| puts value }
+
+#opposites.each { |key, value| puts "The opposite of #{key} is #{value}" }
