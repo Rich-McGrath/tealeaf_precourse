e811d25a45397224b8e5bc38361d8870c79b9b2c
Rich-McGrath <richmcgrath@gmail.com>
2015-07-06 18:14:38 -0400

    intro exercise #9 part 3

--- intro_exercise_9.rb
+++ intro_exercise_9.rb
@@ -2,6 +2,10 @@ h = {a:1, b:2, c:3, d:4}
 
 p h.values_at(:b)
 
-h.store("e", 5)
+h.store(:e, 5)
 
-p h
+p "Hash with new values #{h}"
+
+h.delete_if { |key, value| value < 3.5 }
+
+p "Hash with values below 3.5 removed #{h}"
