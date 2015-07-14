bd34819d9711a692bb1afecd774ca761c1abd68b
Rich-McGrath <richmcgrath@gmail.com>
2015-07-13 20:50:57 -0400

    Quiz 2 part 1

---
+++ quiz_2-1.rb
@@ -0,0 +1,7 @@
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+p ages.key?("Spot")
+
+p ages.include?("Spot") # Bonus answer 1
+
+p ages.member?("Spot") # Bonus answer 2
