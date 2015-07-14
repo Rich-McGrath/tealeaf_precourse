28aaf00236627266f8bbcffe64f4786b346a0faf
Rich-McGrath <richmcgrath@gmail.com>
2015-07-13 22:27:23 -0400

    books answer to quiz 2 part 3

--- quiz_2-3.rb
+++ quiz_2-3.rb
@@ -1,3 +1,5 @@
 ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
 
 p ages.delete_if {|key, value| value >= 100 }
+
+p ages.keep_if { |name, age| age < 100 } # the books answere
