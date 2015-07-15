2538190cbbc32a40f11293fe907fc25d230f68c6
Rich-McGrath <richmcgrath@gmail.com>
2015-07-14 20:21:37 -0400

    Book answer to quiz 2 part 10

--- quiz_2-10.rb
+++ quiz_2-10.rb
@@ -1,2 +1,4 @@
 flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
-flintstones.map! { |e| p e[0..2]  } 
+flintstones.map! { |e| p e[0..2]  }
+
+flintstones.map! { |name| p name[0,3] } #book answer
