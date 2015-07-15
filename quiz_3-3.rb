85d06544d574b4a79b8bbd11f8929914dbbf7b9d
Rich-McGrath <richmcgrath@gmail.com>
2015-07-14 20:34:13 -0400

    Book answer Quiz 3 part 3

--- quiz_3-3.rb
+++ quiz_3-3.rb
@@ -1,5 +1,7 @@
 flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
 
-flintstones.push("Dino", "Hoppy") 
+flintstones.push("Dino", "Hoppy")
+
+# flintstones.push("Dino").push("Hoppy") #Book Answer
 
 p flintstones
